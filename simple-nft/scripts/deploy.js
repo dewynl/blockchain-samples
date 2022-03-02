d678d2bcc7a15360a90b461bb02a18c76726d350
Dewyn liriano <dewyn.liriano@gmail.com>
2022-03-02 00:39:35 -0400

    used hardhat and alchemy's api to create an ECR-721 token

---
+++ simple-nft/scripts/deploy.js
@@ -0,0 +1,18 @@
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
+
+async function main() {
+  const nftContract = await ethers.getContractFactory("RandomNft");
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
